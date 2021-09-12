b5331db97aa23a2c53ba350f60d715ab7c9bdee0
Jang-jiwon <66350779+Jang-jiwon@users.noreply.github.com>
2021-09-12 14:29:04 +0900

    NEW화면완료

--- app/src/main/java/com/example/rezero/NewFragment.java
+++ app/src/main/java/com/example/rezero/NewFragment.java
@@ -2,14 +2,22 @@ package com.example.rezero;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class NewFragment extends Fragment {
+
+    private Context mContext;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -17,8 +25,6 @@ public class NewFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    TextView tedxt1;
 
     public NewFragment() {
         // Required empty public constructor
@@ -33,8 +39,8 @@ public class NewFragment extends Fragment {
      * @return A new instance of fragment AllFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NewFragment newInstance(String param1, String param2) {
-        NewFragment fragment = new NewFragment();
+    public static BestFragment newInstance(String param1, String param2) {
+        BestFragment fragment = new BestFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -51,11 +57,50 @@ public class NewFragment extends Fragment {
         }
     }
 
+    //은수화이팅ㅎ
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View view = inflater.inflate(R.layout.fragment_new, container, false);
+        View view = inflater.inflate(R.layout.fragment_best, container, false);
+
+        //탭레이아웃
+        {mTabLayout = (TabLayout) view.findViewById(R.id.layout_tab_best);
+
+            mTabLayout.addTab(mTabLayout.newTab().setText("전체"));
+            mTabLayout.addTab(mTabLayout.newTab().setText("주방"));
+            mTabLayout.addTab(mTabLayout.newTab().setText("욕실"));
+            mTabLayout.addTab(mTabLayout.newTab().setText("리빙"));
+            mTabLayout.addTab(mTabLayout.newTab().setText("문구"));
+            mTabLayout.addTab(mTabLayout.newTab().setText("책"));
+            mTabLayout.addTab(mTabLayout.newTab().setText("음식"));
+            mTabLayout.addTab(mTabLayout.newTab().setText("화장품"));
+            mTabLayout.addTab(mTabLayout.newTab().setText("반려동물"));
+            mTabLayout.addTab(mTabLayout.newTab().setText("여성용품"));
+
+
+            mViewPager = (ViewPager) view.findViewById(R.id.pager_content_best);
+            BestVPagerAdapter mContentsPagerAdapter = new BestVPagerAdapter(
+                    getChildFragmentManager(), mTabLayout.getTabCount());
+            mViewPager.setAdapter(mContentsPagerAdapter);
+
+            mViewPager.addOnPageChangeListener(
+                    new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+            mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    mViewPager.setCurrentItem(tab.getPosition());
+                }
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+                }
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+                }
+            });}
 
 
 

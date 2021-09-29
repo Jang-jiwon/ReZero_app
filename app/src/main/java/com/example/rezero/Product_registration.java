450e29844d1c05231eddb2a51f7a7e32a1e0e59a
yoonui <yoony0612@ajou.ac.kr>
2021-09-29 20:23:12 +0900

    상품등록 화면 진행중

---
+++ app/src/main/java/com/example/rezero/Product_registration.java
@@ -0,0 +1,14 @@
+package com.example.rezero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Product_registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_registration);
+    }
+}

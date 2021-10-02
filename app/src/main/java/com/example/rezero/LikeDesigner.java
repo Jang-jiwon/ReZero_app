01b0f7e21d4676f128cb968fbc0f366a4a4dafc3
Jang-jiwon <66350779+Jang-jiwon@users.noreply.github.com>
2021-10-02 11:08:00 +0900

    다시

---
+++ app/src/main/java/com/example/rezero/LikeDesigner.java
@@ -0,0 +1,82 @@
+package com.example.rezero;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LikeDesigner#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LikeDesigner extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public LikeDesigner() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LikeDesigner.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LikeDesigner newInstance(String param1, String param2) {
+        LikeDesigner fragment = new LikeDesigner();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    LinearLayout likeDeLay;
+    View view;
+    int DESIGNER_NUM = 10;
+    View[] like_profiles = new View[DESIGNER_NUM];
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_like_designer, container, false);
+
+        likeDeLay = (LinearLayout)view.findViewById(R.id.likeDeLay);
+
+
+
+
+
+        return view;
+    }
+}

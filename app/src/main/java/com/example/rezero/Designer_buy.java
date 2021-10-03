77973af1dc3edcf6bab161c90a60c3b3fee05c1f
yoonui <yoony0612@ajou.ac.kr>
2021-10-03 22:53:25 +0900

    디자이너 결제목록 주문 상세 약간 구현

--- app/src/main/java/com/example/rezero/Designer_buy.java
+++ app/src/main/java/com/example/rezero/Designer_buy.java
@@ -3,6 +3,7 @@ package com.example.rezero;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +19,7 @@ public class Designer_buy extends AppCompatActivity {
 
     LinearLayout main_layout, detail_layout;
     ImageButton clear_choice2, clear_choice;
-    ImageView line1, line2;
+    ImageView line1, line2, back;
 
     int cartsNum=8;
     View[] profiles = new View[cartsNum];
@@ -33,6 +34,15 @@ public class Designer_buy extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_designer_buy);
 
+        // 뒤로가기
+        back = (ImageView) findViewById(R.id.backBtn);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
 
         //스크롤뷰 제일 아래에 깔리는 배경 레이아웃
         main_layout = (LinearLayout) findViewById(R.id.main_layout);
@@ -42,6 +52,13 @@ public class Designer_buy extends AppCompatActivity {
             profiles[i] = mInflater.inflate(R.layout.designer_buy_profile_dyes, null);//프로파일레이아웃을 인플레이트
             checkBoxes[i] = (CheckBox) profiles[i].findViewById(R.id.check);
             detail_button[i] = (Button) profiles[i].findViewById(R.id.detail_button);
+            detail_button[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Designer_buy.this, designer_buy_detail.class);
+                    startActivity(intent);
+                }
+            });
             textView[i] = (TextView)profiles[i].findViewById(R.id.d_yes_no);
         }
 
@@ -54,6 +71,13 @@ public class Designer_buy extends AppCompatActivity {
             profiles[i] = mInflater.inflate(R.layout.designer_buy_profile_dno, null);//프로파일레이아웃을 인플레이트
             checkBoxes[i] = (CheckBox) profiles[i].findViewById(R.id.check);
             detail_button[i] = (Button) profiles[i].findViewById(R.id.detail_button);
+            detail_button[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Designer_buy.this, designer_buy_detail.class);
+                    startActivity(intent);
+                }
+            });
             textView[i] = (TextView)profiles[i].findViewById(R.id.d_yes_no);
         }
 

1602a798a2068fcaf583ec6d948cbf3c4625d0f4
yoonui <yoony0612@ajou.ac.kr>
2021-10-03 22:15:07 +0900

    쿠폰 및 포인트 화면 뒤로가기 기능 추가

--- app/src/main/java/com/example/rezero/point.java
+++ app/src/main/java/com/example/rezero/point.java
@@ -3,12 +3,24 @@ package com.example.rezero;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class point extends AppCompatActivity {
 
+    private ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_point);
+
+        // 뒤로가기
+        back = (ImageView) findViewById(R.id.backBtn);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }

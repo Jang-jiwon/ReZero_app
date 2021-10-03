991a749049df4e8cb4ef8670a654bb977289d432
yoonui <yoony0612@ajou.ac.kr>
2021-10-03 13:09:37 +0900

    디자이너 결제목록 화면(16-1) 진행중 - 기능구현중

--- app/src/main/java/com/example/rezero/Designer_buy.java
+++ app/src/main/java/com/example/rezero/Designer_buy.java
@@ -2,13 +2,51 @@ package com.example.rezero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Designer_buy extends AppCompatActivity {
+
+    LinearLayout main_layout;
+
+    int cartsNum=4;
+    View[] profiles = new View[cartsNum];
+    CheckBox[] checkBoxes = new CheckBox[cartsNum];
+    Button[] plusBtns=new Button[cartsNum];
+    Button[] minusBtns=new Button[cartsNum];
+    TextView[] numTxvs = new TextView[cartsNum];
+    CheckBox allCheck;
+    TextView[] prices = new TextView[cartsNum];//선택상품가격
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_designer_buy);
+
+
+        //스크롤뷰 제일 아래에 깔리는 배경 레이아웃
+        main_layout = (LinearLayout) findViewById(R.id.main_layout);
+        LayoutInflater mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        for (int i=0;i<cartsNum;i++){
+            profiles[i] = mInflater.inflate(R.layout.designer_buy_profile, null);//프로파일레이아웃을 인플레이트
+            checkBoxes[i] = (CheckBox) profiles[i].findViewById(R.id.check);
+            //plusBtns[i] = (Button) profiles[i].findViewById(R.id.plusbtn);
+            //minusBtns[i] = (Button) profiles[i].findViewById(R.id.minusbtn);
+            //numTxvs[i]=(TextView) profiles[i].findViewById(R.id.numTxv);
+            //prices[i]=(TextView) profiles[i].findViewById(R.id.price);
+        }
+
+        //프로파일레이어붙히기
+        for (int i=0;i<cartsNum;i++){
+            main_layout.addView(profiles[i]);//프로파일레이아웃 붙힘
+        }
+
     }
 }

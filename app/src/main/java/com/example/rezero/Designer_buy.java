6e804c7795e532659a44183706f8236f08c68747
yoonui <yoony0612@ajou.ac.kr>
2021-10-03 20:45:42 +0900

    디자이너 결제목록 화면(16-1) 진행중 - 기능구현중3

--- app/src/main/java/com/example/rezero/Designer_buy.java
+++ app/src/main/java/com/example/rezero/Designer_buy.java
@@ -8,21 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class Designer_buy extends AppCompatActivity {
 
     LinearLayout main_layout;
+    ImageButton clear_choice2;
 
     int cartsNum=4;
     View[] profiles = new View[cartsNum];
     CheckBox[] checkBoxes = new CheckBox[cartsNum];
     Button[] detail_button = new Button[cartsNum];
 
-    TextView[] numTxvs = new TextView[cartsNum];
-    CheckBox allCheck;
-    TextView[] prices = new TextView[cartsNum];//선택상품가격
+    CheckBox allCheck_detail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +35,56 @@ public class Designer_buy extends AppCompatActivity {
         LayoutInflater mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         for (int i=0;i<cartsNum;i++){
-            profiles[i] = mInflater.inflate(R.layout.designer_buy_profile, null);//프로파일레이아웃을 인플레이트
+            profiles[i] = mInflater.inflate(R.layout.designer_buy_profile_dyes, null);//프로파일레이아웃을 인플레이트
             checkBoxes[i] = (CheckBox) profiles[i].findViewById(R.id.check);
             detail_button[i] = (Button) profiles[i].findViewById(R.id.detail_button);
-            //minusBtns[i] = (Button) profiles[i].findViewById(R.id.minusbtn);
-            //numTxvs[i]=(TextView) profiles[i].findViewById(R.id.numTxv);
-            //prices[i]=(TextView) profiles[i].findViewById(R.id.price);
         }
 
         //프로파일레이어붙히기
         for (int i=0;i<cartsNum;i++){
             main_layout.addView(profiles[i]);//프로파일레이아웃 붙힘
         }
+
+        for (int i=0;i<cartsNum;i++){
+            profiles[i] = mInflater.inflate(R.layout.designer_buy_profile_dno, null);//프로파일레이아웃을 인플레이트
+            checkBoxes[i] = (CheckBox) profiles[i].findViewById(R.id.check);
+            detail_button[i] = (Button) profiles[i].findViewById(R.id.detail_button);
+        }
+
+        //프로파일레이어붙히기
+        for (int i=0;i<cartsNum;i++){
+            main_layout.addView(profiles[i]);//프로파일레이아웃 붙힘
+        }
+
+        //전체선택
+        allCheck_detail = (CheckBox) findViewById(R.id.allCheck_detail);
+        allCheck_detail.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b==true){
+                    for (int i=0;i<cartsNum;i++){
+                        checkBoxes[i].setChecked(true);
+                    }
+                }else {
+                    for (int i=0;i<cartsNum;i++){
+                        checkBoxes[i].setChecked(false);
+                    }
+                }
+            }
+        });
+
+        //선택삭제
+        clear_choice2 = (ImageButton) findViewById(R.id.clear_choice2);
+        clear_choice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for (int i=0;i<cartsNum;i++){
+                    if (checkBoxes[i].isChecked()==true){
+                        main_layout.removeView(profiles[i]);
+                    }
+                }
+            }
+        });
 
     }
 }
